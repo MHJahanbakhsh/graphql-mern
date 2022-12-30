7c9e9293222807cd16602600631a0dd7ad42d30c
MohammadHossein Jahanbakhsh <mammadjahanbakhsh@yahoo.com>
2022-12-30 23:34:55 +0330

    add query to get all clients, all projects and a single project with pretaining client

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,6 +1,6 @@
 const {projects,clients} = require('../sampleData')
 
-const {GraphQLObjectType, GraphQLID, GraphQLString, GraphQLSchema, } = require('graphql')
+const {GraphQLObjectType, GraphQLID, GraphQLString, GraphQLSchema, GraphQLList, } = require('graphql')
 
 //Client type
 const ClientType = new GraphQLObjectType({
@@ -14,9 +14,51 @@ const ClientType = new GraphQLObjectType({
 })
 
 
+//Client type
+const ProjectType = new GraphQLObjectType({
+    name:'Project',
+    fields:()=>({
+        id:{type: GraphQLID}, //altough in sampleData, it is just a string number
+        name:{type:GraphQLString},
+        description:{type:GraphQLString},
+        status:{type:GraphQLString},
+        client:{
+            type:ClientType,
+            resolve(parent,args){
+                console.log(parent) //parent is a project
+                return clients.find(client=>client.id ===parent.clientId)
+            }
+        }
+    })
+})
+
+
 const RootQuery = new GraphQLObjectType({
     name:'RootQueryType',
     fields:{
+        projects:{
+            type: new GraphQLList(ProjectType),
+            //we have no args here,hence no argument while addressing that query 
+            resolve(parent,args){
+                return projects
+            }
+        },
+        project:{
+            type:ProjectType,
+            args: {id: {type: GraphQLID}}, //adding args specifies that this query requires argument
+            resolve(parent,args){ //for now this is just a regular function.but later will be a mongoose function
+                console.log(parent) //receive undefined. why? what is parent
+                return projects.find(project=>project.id===args.id)
+            }
+        },
+
+        clients:{
+            type: new GraphQLList(ClientType),
+            //we have no args here,hence no argument while addressing that query 
+            resolve(parent,args){
+                return clients
+            }
+        },
         client:{
             type:ClientType,
             args: {id: {type: GraphQLID}}, //adding args specifies that this query requires argument
