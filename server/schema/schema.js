df91d03b4418c0642feb8d9f4d9bdfd557869ce4
MohammadHossein Jahanbakhsh <mammadjahanbakhsh@yahoo.com>
2022-12-30 22:23:18 +0330

    add query to get single client

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,3 +1,33 @@
 const {projects,clients} = require('../sampleData')
 
-const graphql = require('graphql')
+const {GraphQLObjectType, GraphQLID, GraphQLString, GraphQLSchema, } = require('graphql')
+
+//Client type
+const ClientType = new GraphQLObjectType({
+    name:'Client',
+    fields:()=>({
+        id:{type: GraphQLID}, //altough in sampleData, it is just a string number
+        name:{type:GraphQLString},
+        email:{type:GraphQLString},
+        phone:{type:GraphQLString},
+    })
+})
+
+
+const RootQuery = new GraphQLObjectType({
+    name:'RootQueryType',
+    fields:{
+        client:{
+            type:ClientType,
+            args: {id: {type: GraphQLID}}, //adding args specifies that this query requires argument
+            resolve(parent,args){ //for now this is just a regular function.but later will be a mongoose function
+                console.log(parent) //receive undefined. why? what is parent
+                return clients.find(client=>client.id===args.id)
+            }
+        }
+    }
+})
+
+module.exports = new GraphQLSchema({
+    query:RootQuery
+})
